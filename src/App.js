341fb2e5dc9caa4dddd297a6d997e535b7bbdea1
Rohitilak <rohittilak086@gmail.com>
2024-03-21 10:30:40 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Hugging from './comoenents/Hugging';
+
+function App() {
+  return (
+    <div className="App">
+      <Hugging/>
+    </div>
+  );
+}
+
+export default App;
